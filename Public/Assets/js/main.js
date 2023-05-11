868dbef82f50bbeb93b3c39f9c0405b676651b16
Yanolateam <yanolateam@gmail.com>
2023-05-11 16:23:26 +0200

    debug output

--- Public/Assets/js/main.js
+++ Public/Assets/js/main.js
@@ -1,1 +1,10 @@
 console.log('Hello World');
+
+// complete le champ #content avec le contenu de la variable content
+let content = document.getElementById('content');
+
+// au changement de valeur du champ content, console.log "coucou"
+content.addEventListener('change', function() {
+    console.log('coucou');
+}
+);
