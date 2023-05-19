a3832aa2880630c9d555a65cc3b07e478f8f1c93
Langlet Gaëtan <gtn.langlet@gmail.com>
2023-05-19 07:52:27 +0200

    update insight 3

--- Src/Controller/ContactController.php
+++ Src/Controller/ContactController.php
@@ -23,23 +23,22 @@ class ContactController extends Controller
             $mail->Password   = 'mzeupojmzkynlwbm';                               //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
             $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-        
+
             //Recipients
             $mail->setFrom('contact@gaetan-langlet.fr', 'Mailer');
             $mail->addAddress('gtn.langlet@gmail.com', 'Joe User');     //Add a recipient              //Name is optional
             $mail->addReplyTo('no-reply@gaetan-langlet.fr', 'Information');
-        
+
             //Content
             $mail->isHTML(true);                                  //Set email format to HTML
-            $mail->Subject = 'Mail de contact du blog '.Application::$session->get('username');
+            $mail->Subject = 'Mail de contact du blog ' . Application::$session->get('username');
             $mail->Body = $_POST['message'] . '<br>Envoyé par : ' . $_POST['name'] . '<br>Mail de contact : ' . $_POST['email'];
-        
+
             $mail->send();
             echo 'Message has been sent';
             Application::$app->response->redirect('/articles');
         } catch (Exception $e) {
             echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-
         }
     }
 }
